ce3d9cae22b09b8971e57654fe4963bc797d7d5a
Amir Hasanpour <amirhasanpour.hp10@gmail.com>
2025-10-30 23:31:10 +0330

    Setup GORM and create postgres connection

---
+++ chat-service/pkg/database/postgres.go
@@ -0,0 +1,71 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"chat-service/internal/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func NewPostgresConnection() (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second,
+			LogLevel:      logger.Info,
+			Colorful:      true,
+		},
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get database instance: %w", err)
+	}
+
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	log.Println("Successfully connected to PostgreSQL database")
+	return db, nil
+}
+
+func AutoMigrate(db *gorm.DB) error {
+	err := db.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";").Error
+	if err != nil {
+		return fmt.Errorf("failed to create uuid extension: %w", err)
+	}
+
+	err = db.AutoMigrate(
+		&models.Room{},
+		&models.Message{},
+		&models.RoomMember{},
+	)
+	if err != nil {
+		return fmt.Errorf("failed to auto migrate models: %w", err)
+	}
+
+	log.Println("Chat Service database migration completed successfully")
+	return nil
+}
